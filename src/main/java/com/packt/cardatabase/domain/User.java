c27c8fe2618ea9e901f15c08e85486f064ebec30
Vito Millozza <v.millozza@gmail.com>
2023-09-02 15:51:21 +0200

    Jwt

--- src/main/java/com/packt/cardatabase/domain/User.java
+++ src/main/java/com/packt/cardatabase/domain/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class User {
--- src/main/java/com/packt/cardatabase/service/UserDetailsServiceImpl.java
+++ src/main/java/com/packt/cardatabase/service/UserDetailsServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.packt.cardatabase.domain.User;
 
